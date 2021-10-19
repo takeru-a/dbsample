33ab082dc5e4e23ff0bae3a84ca1e54db5ece1ad
takeru-a <pypy20191022@gmail.com>
2021-10-19 14:12:44 +0900

    first

---
+++ src/main/java/oit/is/team13/work04/dbsample/DbsampleApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team13.work04.dbsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team13/work04/dbsample/DbsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team13.work04.dbsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
